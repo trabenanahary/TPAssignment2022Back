9edad4d98a50e95caab67267e00c30644d0da32f
trabenanahary <trabenanahary@gmail.com>
2022-04-30 00:54:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MBDS_Madagascar_2021_2022Back
---
+++ model/assignment.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+// Pour ajouter la pagination
+AssignmentSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('assignments', AssignmentSchema);
