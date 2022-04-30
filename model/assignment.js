24e668469bafd80e9638d14eeb92d28b34cc6fa7
trabenanahary <trabenanahary@gmail.com>
2022-05-01 00:46:28 +0300

    Gestion de la pagination

--- model/assignment.js
+++ model/assignment.js
@@ -2,6 +2,7 @@ let mongoose = require('mongoose');
 const matiere = require('./matiere');
 const users = require('./users');
 let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 let AssignmentSchema = Schema({
     dateDeRendu: Date,
@@ -18,6 +19,8 @@ let AssignmentSchema = Schema({
     eleveRef: {type: Schema.Types.ObjectId, ref: users},
     matiereRef: {type: Schema.Types.ObjectId, ref: matiere},
 });
+// Pour ajouter la pagination
+AssignmentSchema.plugin(aggregatePaginate);
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
 module.exports = mongoose.model('Assignment', AssignmentSchema);
--- routes/assignments.js
+++ routes/assignments.js
@@ -60,6 +60,7 @@ function getAssignmentsComplete(req, res) {
 }
 
 // Récupérer tous les assignments (GET)
+/*
 function getAssignments(req, res){
     Assignment.find((err, assignments) => {
         if(err){
@@ -69,7 +70,22 @@ function getAssignments(req, res){
         res.send(assignments);
     });
 }
-
+*/
+function getAssignments(req, res) {
+    var aggregateQuery = Assignment.aggregate();
+    Assignment.aggregatePaginate(aggregateQuery,
+      {
+        page: parseInt(req.query.page) || 1,
+        limit: parseInt(req.query.limit) || 10,
+      },
+      (err, assignments) => {
+        if (err) {
+          res.send(err);
+        }
+        res.send(assignments);
+      }
+    );
+   }
 // Récupérer un assignment par son id (GET)
 function getAssignment(req, res){
     let assignmentId = req.params.id;
